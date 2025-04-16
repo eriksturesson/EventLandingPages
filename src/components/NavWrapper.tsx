9d819d9739af9afb841dd722b89d424910947f6a
Simoneiv <simone.ivars99@gmail.com>
2025-04-16 16:05:02 +0200

    navWrapper does not work yet but started to implement mui

--- src/components/NavWrapper.tsx
+++ src/components/NavWrapper.tsx
@@ -2,6 +2,14 @@
 //import { Navbar, Nav, Container } from 'react-bootstrap';
 //import arrangerandeKlubbarHTML from "../"
 
+import { Box, Button, Link } from '@mui/material';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 /*
             <div className="right-menu-in-nav" id="reactNavbar">
                 <Navbar bg="primary" variant="dark">
@@ -20,20 +28,42 @@
 
 const NavWrapper = ({ websiteID }: { websiteID: string }): JSX.Element => {
    return (
-      <div className="eriks-nav-wrapper">
-         <ul className="right-menu-in-nav">
-            <li>
-               <a href="/">Home</a>
-            </li>
-            <li>
-               <a href="./tidigareprogram">Föregående år</a>
-            </li>
-            <li>
-               <a href="./arrangerandeklubbar">Arrangörer</a>
-            </li>
-         </ul>
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+         <AppBar position="static">
+            <Toolbar variant="dense">
+               <Button component="a" href="/" target="_blank" rel="noopener noreferrer">
+                  Home
+               </Button>
+               <Link href="/">Home</Link>
+               <Link href="./tidigareprogram">Föregående år</Link>
+               <Link href="./arrangerandeklubbar">Arrangörer</Link>
+               <Typography variant="h6" color="inherit" component="div">
+                  Photos
+               </Typography>
+
+               <Typography variant="h6" color="inherit" component="div">
+                  Photos
+               </Typography>
+            </Toolbar>
+         </AppBar>
+      </Box>
    );
 };
+
+// return (
+//    <Box >
+//       <ul >
+//          <li>
+//             <a href="/">Home</a>
+//          </li>
+//          <li>
+//             <a href="./tidigareprogram">Föregående år</a>
+//          </li>
+//          <li>
+//             <a href="./arrangerandeklubbar">Arrangörer</a>
+//          </li>
+//       </ul>
+//    </Box>
+// );
 
 export default NavWrapper;
