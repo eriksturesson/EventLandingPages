4697b36b85d6b56bd285dc099c851116b1494cda
Erik Sturesson <hej@eriksturesson.se>
2025-06-07 11:39:32 +0200

    starting to add editing logic for admins in navbar

--- src/components/NavWrapper.tsx
+++ src/components/NavWrapper.tsx
@@ -1,8 +1,5 @@
-//import { BrowserRouter, Link, Route } from "react-router-dom";
-//import { Navbar, Nav, Container } from 'react-bootstrap';
-//import arrangerandeKlubbarHTML from "../"
-
 import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Box, Button, Drawer, IconButton, List, ListItem, ListItemText, Modal, TextField, Typography } from '@mui/material';
 import { useState } from 'react';
@@ -11,42 +8,66 @@ import logga from '../assets/LOGGA FÄRDIGT UTKAST.jpg';
 import { useDbContent } from '../contexts/DBContentContext';
 import { PageMetadata } from '../interfaces/dbInterfaces';
 import { readAndWriteToFirebase } from '../utils/firebaseFunctions';
-const style = {
+
+const modalStyle = {
    position: 'absolute' as const,
    top: '50%',
    left: '50%',
    transform: 'translate(-50%, -50%)',
-   width: 300,
+   width: 400,
    bgcolor: 'background.paper',
    boxShadow: 24,
    borderRadius: 2,
    p: 4,
 };
+
 const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
    const { customPageMetaData, setCustomPagesMetaData, websiteID } = useDbContent();
    const [menuOpen, setMenuOpen] = useState(false);
    const [modalOpen, setModalOpen] = useState(false);
+   const [isEditMode, setIsEditMode] = useState(false);
+   const [editingItemID, setEditingItemID] = useState<string | null>(null);
    const [newItem, setNewItem] = useState<PageMetadata>({ pageName: '', pageLink: '', pageID: '' });
-   const handleDrawerToggle = () => setMenuOpen(!menuOpen);
-   const handleModalToggle = () => setModalOpen(!modalOpen);
 
-   const handleAddItem = async () => {
-      if (newItem.pageName && newItem.pageLink) {
-         let newPageID = uuidv4();
-         newItem.pageID = newPageID;
-         //First update db
-         try {
-            await readAndWriteToFirebase({
-               method: 'update',
-               ref: `websites/${websiteID}/customPages/${newPageID}/metadata/`,
-               data: newItem,
-            });
-            //If successful, also update state
-            setCustomPagesMetaData([...customPageMetaData, newItem]);
-            setModalOpen(false);
-         } catch (error) {
-            console.error('Error adding new item:', error);
+   const toggleDrawer = () => setMenuOpen(!menuOpen);
+
+   const openModal = (item?: PageMetadata) => {
+      if (item) {
+         setIsEditMode(true);
+         setEditingItemID(item.pageID);
+         setNewItem(item);
+      } else {
+         setIsEditMode(false);
+         setEditingItemID(null);
+         setNewItem({ pageName: '', pageLink: '', pageID: '' });
+      }
+      setModalOpen(true);
+   };
+
+   const handleSaveItem = async () => {
+      if (!newItem.pageName || !newItem.pageLink) return;
+
+      const isNew = !isEditMode;
+      const pageID = isNew ? uuidv4() : editingItemID!;
+      const itemToSave = { ...newItem, pageID };
+
+      try {
+         await readAndWriteToFirebase({
+            method: 'update',
+            ref: `websites/${websiteID}/customPages/${pageID}/metadata/`,
+            data: itemToSave,
+         });
+
+         if (isNew) {
+            setCustomPagesMetaData([...customPageMetaData, itemToSave]);
+         } else {
+            const updated = customPageMetaData.map((item) => (item.pageID === pageID ? itemToSave : item));
+            setCustomPagesMetaData(updated);
          }
+
+         setModalOpen(false);
+      } catch (error) {
+         console.error('Error saving item:', error);
       }
    };
 
@@ -56,49 +77,37 @@ const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
             backgroundColor: 'transparent',
             backdropFilter: 'blur(8px)',
             marginTop: -1,
-            marinLeft: 0,
-            marginRight: 0,
             paddingBottom: 0,
             flexGrow: 1,
             boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
             position: 'relative',
          }}
       >
-         <Box
-            sx={{
-               display: 'flex',
-               alignItems: 'center',
-               justifyContent: 'space-between',
-               paddingX: 2,
-            }}
-         >
+         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', px: 2 }}>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-               <Box
-                  sx={{
-                     backgroundColor: 'white',
-                     paddingX: '24px',
-                     paddingY: '4px',
-                     borderRadius: '12px',
-                     display: 'inline-block',
-                  }}
-               >
+               <Box sx={{ backgroundColor: 'white', px: 3, py: 0.5, borderRadius: 3 }}>
                   <img src={logga} alt="Logo" style={{ width: '120px', height: '50px' }} />
                </Box>
             </Box>
+
+            {/* Desktop Menu */}
             <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
                {isAdmin && (
-                  <Button startIcon={<AddIcon />} variant="contained" onClick={handleModalToggle}>
+                  <Button startIcon={<AddIcon />} variant="contained" onClick={() => openModal()}>
                      Add Link
                   </Button>
                )}
                {customPageMetaData.map((item) => (
                   <Button
                      key={item.pageID}
-                     component="a"
-                     href={item.pageLink}
+                     startIcon={isAdmin ? <EditIcon /> : null}
+                     component={isAdmin ? 'button' : 'a'}
+                     onClick={isAdmin ? () => openModal(item) : undefined}
+                     href={isAdmin ? undefined : item.pageLink}
                      target="_blank"
                      rel="noopener noreferrer"
                      sx={{
+                        border: isAdmin ? '1px solid black' : 'none',
                         color: 'black',
                         fontWeight: 'bold',
                         textTransform: 'none',
@@ -111,23 +120,27 @@ const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
                   </Button>
                ))}
             </Box>
+
+            {/* Mobile Menu Icon */}
             <IconButton
                sx={{
                   display: { xs: 'flex', md: 'none' },
                   color: 'black',
                   border: '1px solid black',
                   borderRadius: '12px',
-                  padding: 1,
+                  p: 1,
                   backgroundColor: 'rgba(255, 255, 255, 0.7)',
                }}
-               onClick={handleDrawerToggle}
+               onClick={toggleDrawer}
             >
                <MenuIcon fontSize="medium" />
             </IconButton>
+
+            {/* Drawer (Mobile Menu) */}
             <Drawer
                anchor="right"
                open={menuOpen}
-               onClose={handleDrawerToggle}
+               onClose={toggleDrawer}
                PaperProps={{
                   sx: { width: 250, backgroundColor: 'primary.main', color: 'white' },
                }}
@@ -135,7 +148,10 @@ const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
                <List>
                   {isAdmin && (
                      <IconButton
-                        onClick={handleModalToggle}
+                        onClick={() => {
+                           toggleDrawer();
+                           openModal();
+                        }}
                         sx={{
                            position: 'absolute',
                            top: 12,
@@ -151,7 +167,8 @@ const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
                      </IconButton>
                   )}
                   {customPageMetaData.map((item) => (
-                     <ListItem button key={item.pageName} component="a" href={item.pageLink} onClick={handleDrawerToggle}>
+                     <ListItem key={item.pageID} component="a" href={item.pageLink} onClick={toggleDrawer}>
+                        <EditIcon sx={{ mr: 1 }} />
                         <ListItemText primary={item.pageName} />
                      </ListItem>
                   ))}
@@ -159,23 +176,12 @@ const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
             </Drawer>
          </Box>
 
+         {/* Modal */}
          {isAdmin && (
-            <Modal open={modalOpen} onClose={handleModalToggle}>
-               <Box
-                  sx={{
-                     position: 'absolute',
-                     top: '50%',
-                     left: '50%',
-                     transform: 'translate(-50%, -50%)',
-                     width: 400,
-                     bgcolor: 'background.paper',
-                     boxShadow: 24,
-                     borderRadius: 2,
-                     p: 4,
-                  }}
-               >
+            <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+               <Box sx={modalStyle}>
                   <Typography variant="h6" mb={2}>
-                     Lägg till länk
+                     {isEditMode ? 'Redigera länk' : 'Lägg till länk'}
                   </Typography>
                   <TextField
                      fullWidth
@@ -193,8 +199,8 @@ const NavWrapper = ({ isAdmin }: { isAdmin: boolean }): JSX.Element => {
                      onChange={(e) => setNewItem({ ...newItem, pageLink: e.target.value })}
                      sx={{ mb: 2 }}
                   />
-                  <Button fullWidth variant="contained" onClick={handleAddItem}>
-                     Lägg till
+                  <Button fullWidth variant="contained" onClick={handleSaveItem}>
+                     {isEditMode ? 'Spara ändringar' : 'Lägg till'}
                   </Button>
                </Box>
             </Modal>
