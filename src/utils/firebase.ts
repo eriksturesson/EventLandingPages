0eaae9efc46a1c5795c59db91bda7bf3338bf33c
Erik Sturesson <hej@eriksturesson.se>
2025-06-08 17:55:16 +0200

    fixing type misstake of databaseURL

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -34,7 +34,7 @@ if (devSettings === 'development') {
    config = {
       apiKey: firebaseConfig.apiKey,
       authDomain: firebaseConfig.apiKey,
-      databaseURL: firebaseConfig.apiKey,
+      databaseURL: firebaseConfig.databaseURL,
       projectId: firebaseConfig.projectId,
       storageBucket: firebaseConfig.storageBucket,
       messagingSenderId: firebaseConfig.messagingSenderId,
