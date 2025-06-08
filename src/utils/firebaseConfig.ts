1748e1a5d177a209ed0e2b78eefab18ca1b33dc2
Erik Sturesson <hej@eriksturesson.se>
2025-06-08 13:10:21 +0200

    fixing .env for github actions

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-   apiKey: 'AIzaSyAdlXIpN_ZTtEPwTYtifb9XD9XbmkTGLhw',
-   authDomain: 'skalbar-eventplattform-med-cms.firebaseapp.com',
-   databaseURL: 'https://skalbar-eventplattform-med-cms-default-rtdb.europe-west1.firebasedatabase.app',
-   projectId: 'skalbar-eventplattform-med-cms',
-   storageBucket: 'skalbar-eventplattform-med-cms.appspot.com',
-   messagingSenderId: '427169745740',
-   appId: '1:427169745740:web:d0194cb2af4162b7721de9',
-   measurementId: 'G-XFZK0C34TR',
+   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+   databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
