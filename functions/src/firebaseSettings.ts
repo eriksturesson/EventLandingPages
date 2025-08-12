47891ab543b0092414bc6e5b4aaf8aa63dd93968
Erik Sturesson (Beelink) <hej@eriksturesson.se>
2025-08-12 12:30:33 +0200

    own backend .env variables, not named VITE_...

--- functions/src/firebaseSettings.ts
+++ functions/src/firebaseSettings.ts
@@ -3,16 +3,16 @@ import { getAuth } from 'firebase-admin/auth';
 import { getDatabase } from 'firebase-admin/database';
 
 const isDev = process.env.NODE_ENV !== 'production';
-console.log('VITE_FIREBASE_DATABASE_URL:', process.env.VITE_FIREBASE_DATABASE_URL);
-console.log('VITE_FIREBASE_PROJECT_ID:', process.env.VITE_FIREBASE_PROJECT_ID);
+console.log('VITE_FIREBASE_DATABASE_URL:', process.env.DATABASE_URL);
+console.log('VITE_FIREBASE_PROJECT_ID:', process.env.PROJECT_ID);
 
 const firebaseConfig = isDev
    ? {
         databaseURL: 'http://localhost:9000?ns=emulator',
      }
    : {
-        projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-        databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        databaseURL: process.env.DATABASE_URL,
      };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
