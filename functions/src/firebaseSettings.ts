f2f1941fbcac1048d560f7fe7d0d43d7c812ce02
Erik Sturesson (Beelink) <hej@eriksturesson.se>
2025-08-12 13:18:28 +0200

    optimizing yml again

--- functions/src/firebaseSettings.ts
+++ functions/src/firebaseSettings.ts
@@ -1,4 +1,4 @@
-import { App, getApps, initializeApp } from 'firebase-admin/app';
+import { App, getApps, initializeApp, cert } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getDatabase } from 'firebase-admin/database';
 import dotenv from 'dotenv';
@@ -11,18 +11,27 @@ const databaseUrl = process.env.DATABASE_URL;
 console.log('Project ID:', projectId);
 console.log('Database URL:', databaseUrl);
 
+if (!isDev) {
+   if (!projectId) throw new Error('Missing PROJECT_ID environment variable!');
+   if (!databaseUrl) throw new Error('Missing DATABASE_URL environment variable!');
+   if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+      throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_JSON environment variable!');
+   }
+}
+
 const firebaseConfig = isDev
    ? {
         databaseURL: 'http://localhost:9000?ns=emulator',
      }
    : {
-        projectId: process.env.PROJECT_ID,
-        databaseURL: process.env.DATABASE_URL,
+        credential: cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON!)),
+        projectId,
+        databaseURL: databaseUrl,
      };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+const app: App = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-console.log('Firebase Admin initialized with projectId:', app?.options?.projectId);
+console.log('Firebase Admin initialized with projectId:', app.options.projectId);
 
 const db = getDatabase(app);
 
