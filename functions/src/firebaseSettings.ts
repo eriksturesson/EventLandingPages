c5c094ecadf32d4346203428e4cef471e684cad1
Erik Sturesson (Beelink) <hej@eriksturesson.se>
2025-08-12 14:07:04 +0200

    creation of .env work, fixing more configs

--- functions/src/firebaseSettings.ts
+++ functions/src/firebaseSettings.ts
@@ -1,31 +1,31 @@
-import { App, getApps, initializeApp, cert, applicationDefault } from 'firebase-admin/app';
+import { App, getApps, initializeApp, applicationDefault } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getDatabase } from 'firebase-admin/database';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 const isDev = process.env.NODE_ENV !== 'production';
 const projectId = process.env.PROJECT_ID;
 const databaseUrl = process.env.DATABASE_URL;
+
 console.log('Relevant environment variables:', {
    NODE_ENV: process.env.NODE_ENV,
-   PROJECT_ID: process.env.PROJECT_ID,
-   DATABASE_URL: process.env.DATABASE_URL,
-   FIREBASE_SERVICE_ACCOUNT_JSON: process.env.FIREBASE_SERVICE_ACCOUNT_JSON ? '[SET]' : '[NOT SET]',
+   PROJECT_ID: projectId,
+   DATABASE_URL: databaseUrl,
+   GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS ?? '[NOT SET]',
 });
 
 if (!isDev) {
    if (!projectId) throw new Error('Missing PROJECT_ID environment variable!');
    if (!databaseUrl) throw new Error('Missing DATABASE_URL environment variable!');
-   if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
-      throw new Error('Missing FIREBASE_SERVICE_ACCOUNT_JSON environment variable!');
+   if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+      throw new Error('Missing GOOGLE_APPLICATION_CREDENTIALS environment variable!');
    }
 }
 
 const firebaseConfig = isDev
-   ? {
-        databaseURL: 'http://localhost:9000?ns=emulator',
-     }
+   ? { databaseURL: 'http://localhost:9000?ns=emulator' }
    : {
         credential: applicationDefault(),
         databaseURL: databaseUrl,
